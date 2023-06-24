a4f07ac124e9a242ab99c3d7dca196160eb3540a
Yusun Choi <nsuy.ch@gmail.com>
2023-06-24 21:51:07 +0900

    Add 최유선 과제

---
+++ 3주차 과제/최유선/util/QuizProvider.java
@@ -0,0 +1,58 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class QuizProvider {
+    private final ArrayList<Member> members;
+    private final Integer memberCount;
+
+    public QuizProvider(ArrayList<Member> members) {
+        this.members = members;
+        this.memberCount = members.size();
+    }
+
+    public ArrayList<ArrayList<Integer>> assignNewQuiz() {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        ArrayList<Integer> quizList = new ArrayList<>();
+
+        for (Member member : members) {
+            quizList.addAll(member.getNewQuizList());
+
+            result.add(new ArrayList<>());
+        }
+
+        for (int quiz : quizList) {
+            int randMem;
+            do {
+                randMem = (int) (Math.random() * memberCount);
+            } while (members.get(randMem).getNewQuizList().contains(quiz) || (result.get(randMem).size() >= members.get(randMem).getNewQuizList().size()));
+
+            result.get(randMem).add(quiz);
+        }
+
+        return result;
+    }
+
+    public ArrayList<ArrayList<Integer>> assignReviewQuiz() {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        ArrayList<Integer> quizList = new ArrayList<>();
+
+        for (Member member : members) {
+            quizList.addAll(member.getReviewQuizList());
+
+            result.add(new ArrayList<>());
+        }
+
+        for (int quiz : quizList) {
+            int randMem;
+            do {
+                randMem = (int) (Math.random() * memberCount);
+            } while (members.get(randMem).getReviewQuizList().contains(quiz) || (result.get(randMem).size() >= members.get(randMem).getReviewQuizList().size()));
+
+            result.get(randMem).add(quiz);
+        }
+
+        return result;
+    }
+}
